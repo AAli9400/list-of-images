99870687b90b4ead3fa27c7576db39923ca92b46
Abdel-Rahman Ali <abdelrahman.ali.9400@gmail.com>
2022-06-16 18:31:49 +0200

    make sure Glide will not cache images in memory and only cache them in desk

--- app/src/main/java/com/example/listofimages/ui/util/BindingUtil.kt
+++ app/src/main/java/com/example/listofimages/ui/util/BindingUtil.kt
@@ -3,6 +3,7 @@ package com.example.listofimages.ui.util
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions.withCrossFade
 import com.example.listofimages.R
 
@@ -13,10 +14,14 @@ fun setImage(image: ImageView, url: String?) {
             .load(url)
             .transition(withCrossFade())
             .error(R.drawable.ic_baseline_broken_image_24)
+            .skipMemoryCache(true)
+            .diskCacheStrategy( DiskCacheStrategy.DATA)
             .into(image)
     } else {
         Glide.with(image.context)
             .load(R.drawable.ic_baseline_broken_image_24)
+            .skipMemoryCache(true)
+            .diskCacheStrategy( DiskCacheStrategy.NONE)
             .transition(withCrossFade())
             .into(image)
     }
